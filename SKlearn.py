8939c975a5f38de9a0443fc5bcadcc19c649a646
jncinlee <jncinlee@hotmail.com>
2017-02-21 21:48:30 +0100

    Update SKlearn.py

--- SKlearn.py
+++ SKlearn.py
@@ -35,7 +35,7 @@ X,y = datasets.make_regression(n_samples=100,n_features=1,n_targets=1,noise=10)
 plt.scatter(X,y)
 plt.show()
 plt.savfig('c:/user/lala.png', format='png')
-
+plt.xkcd() #different hand-write style plot
 
 
 ##6 Model
